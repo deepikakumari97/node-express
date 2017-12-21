ba679a2b8f93bf0459c1c3653dfb67bffd5a5197
Deepika Kumari <deepika.dk97@gmail.com>
2017-12-22 02:02:06 +0530

    served static html files using express

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const app = express();
 
 app.use(morgan('dev'));
 
+app.use(express.static(__dirname+'/public'));
+
 app.use((req, res, next) => {
     res.statusCode =200;
     res.setHeader("Content-Type","text/html");
